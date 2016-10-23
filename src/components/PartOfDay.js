fada1aef140a1c58745057f520409da45aa1ab97
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-23 19:14:33 +0300

    Get right background color

--- src/components/PartOfDay.js
+++ src/components/PartOfDay.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+const getBackgroundColor = (title) => {
+  switch (title) {
+    case 'Morning':
+      return '#e3bb88';
+    case 'Day':
+      return '#db9864';
+    case 'Evening':
+      return '#b1695a';
+    case 'Night':
+      return '#644749';
+    default:
+      return '#ffffff';
+  }
+}
+
 const PartOfDay = ({ title, partOfDay }) => {
   return (
-    <View>
+    <View style={{ backgroundColor: getBackgroundColor(title) }}>
       <Text>{title}</Text>
       <Text>{partOfDay.main.temp}</Text>
     </View>
