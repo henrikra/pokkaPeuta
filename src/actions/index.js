b2640be6847d6d12b9e8195f881bb63775606e2d
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-23 18:03:40 +0300

    Make request to weather api With current location

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,27 @@
 import types from '../constants/actionTypes';
+import config from '../config';
 
 export function fetchLocation(coordinates) {
   return (dispatch) => {
     dispatch({ type: types.FETCH_LOCATION });
     navigator.geolocation.getCurrentPosition(
-      ({ coords }) => dispatch({
-        type: types.RECEIVE_LOCATION,
-        coords,
-      }),
+      ({ coords }) => {
+        dispatch({
+          type: types.RECEIVE_LOCATION,
+          coords,
+        });
+
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?appid=${config.apiKey}&units=metric&lat=${coords.latitude}&lon=${coords.longitude}`)
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log('vastaus', responseJson)
+            return responseJson;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+
+      },
       (error) => alert(JSON.stringify(error)),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
