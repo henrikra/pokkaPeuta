cf66a036ddbc564f225065b0d0738527f5d9497a
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-23 20:17:08 +0300

    Add container for image/icon

--- src/components/PartOfDay.js
+++ src/components/PartOfDay.js
@@ -18,14 +18,27 @@ const getBackgroundColor = (title) => {
 
 const PartOfDay = ({ title, partOfDay }) => {
   return (
-    <View style={{ backgroundColor: getBackgroundColor(title) }}>
-      <Text style={styles.title}>{title.toUpperCase()}</Text>
-      <Text style={styles.temperatures}>{Math.round(partOfDay.main.temp)}</Text>
+    <View style={{ ...styles.container, backgroundColor: getBackgroundColor(title) }}>
+      <View style={styles.imageContainer}><Text>lol</Text></View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.title}>{title.toUpperCase()}</Text>
+        <Text style={styles.temperatures}>{Math.round(partOfDay.main.temp)}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = {
+  container: {
+    padding: 15,
+    flexDirection: 'row',
+  },
+  imageContainer: {
+    flex: 3
+  },
+  infoContainer: {
+    flex: 2
+  },
   title: {
     color: 'rgba(255, 255, 255, 0.7)',
     fontSize: 16,
