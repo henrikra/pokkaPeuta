d4b337739c2c3bf297763f7a2d12b8986fe7931a
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-23 12:28:27 +0300

    Change to class based component

--- src/components/WeatherToday.js
+++ src/components/WeatherToday.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
-const WeatherToday = (props) => {
-  return (
-    <Text>I am weather today</Text>
-  );
+class WeatherToday extends Component {
+  render() {
+    return (
+      <Text>I am weather today</Text>
+    );
+  }
 };
 
 const mapStateToProps = ({ geolocation }) => ({
