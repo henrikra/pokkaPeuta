2a9e1de4f144d33a440ce9acc9247c0e00a41ffd
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-23 12:31:08 +0300

    Get user location on componentDidMount

--- src/components/WeatherToday.js
+++ src/components/WeatherToday.js
@@ -3,6 +3,16 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 class WeatherToday extends Component {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('currentlocation', position.coords);
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  }
+
   render() {
     return (
       <Text>I am weather today</Text>
