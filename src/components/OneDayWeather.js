535e62259d3fdb5a3cfd54040f76ac3955131e0b
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-25 23:00:22 +0300

    Remove old function

--- src/components/OneDayWeather.js
+++ src/components/OneDayWeather.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import _ from 'lodash';
-import moment from 'moment';
 
 import PartOfDay from './PartOfDay';
-
-const isAfterToday = (forecast) => {
-  const lastSecondOfToday = moment().set({ hour: 23, minute: 59, second: 59 });
-
-  return moment(forecast.date).isAfter(lastSecondOfToday);
-}
 
 const getTitle = (partOfDay) => {
   const time = partOfDay.dt_txt.split(' ').pop()
@@ -27,10 +19,6 @@ const getTitle = (partOfDay) => {
 }
 
 const OneDayWeather = ({ oneDayWeather }) => {
-  if (isAfterToday(oneDayWeather)) {
-    return null;
-  }
-
   return (
     <View>
       {oneDayWeather.forecast.map((partOfDay, index) => (
