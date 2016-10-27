9f77f6b822bd58fa382606b3959addf797f27798
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-27 21:05:19 +0300

    Remove unused import

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import { ScrollView, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
