ea2ce0d74926f10c493d1c712f8a82a447b55917
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-29 19:03:46 +0300

    Fix lint errors

--- src/reducers/weatherReport.js
+++ src/reducers/weatherReport.js
@@ -15,7 +15,7 @@ const initialState = {
   selectedForecast: null,
 };
 
-export default function(state = initialState, action) {
+export default (state = initialState, action) => {
   switch (action.type) {
     case types.RECEIVE_FORECAST:
       const forecastGroupedByDate = _(action.forecast.list)
@@ -28,16 +28,22 @@ export default function(state = initialState, action) {
           forecast.dt_txt.split(' ').shift()
         )
         .map((forecast, date) => {
-          const dayQuarters = (every3Hour) => _.includes(goodTimes, every3Hour.dt_txt.split(' ').pop());
+          const dayQuarters = every3Hour => _.includes(goodTimes, every3Hour.dt_txt.split(' ').pop());
           return { date, forecast: forecast.filter(dayQuarters) };
         })
-        .filter((oneDayWeather) => oneDayWeather.forecast.length)
+        .filter(oneDayWeather => oneDayWeather.forecast.length)
         .value();
 
-      return { ...state, forecast, selectedForecast: _.first(forecastGroupedByDate), forecastGroupedByDate, city: action.forecast.city };
+      return {
+        ...state,
+        forecast,
+        selectedForecast: _.first(forecastGroupedByDate),
+        forecastGroupedByDate,
+        city: action.forecast.city,
+      };
     case types.SELECT_DATE:
       return { ...state, selectedForecast: action.forecast };
     default:
       return state;
   }
-}
+};
