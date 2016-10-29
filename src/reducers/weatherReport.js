7bb32b6d09014be79f43579e52317147bd229d19
Henrik Raitasola <henrik.raitasola@siili.com>
2016-10-29 19:12:12 +0300

    Remove old code

--- src/reducers/weatherReport.js
+++ src/reducers/weatherReport.js
@@ -2,13 +2,6 @@ import _ from 'lodash';
 
 import types from '../constants/actionTypes';
 
-const goodTimes = [
-  '06:00:00',
-  '12:00:00',
-  '18:00:00',
-  '00:00:00',
-];
-
 const initialState = {
   forecast: [],
   forecastGroupedByDate: [],
@@ -23,20 +16,8 @@ export default (state = initialState, action) => {
         .map((forecast, date) => ({ date, forecast }))
         .value();
 
-      const forecast = _(action.forecast.list)
-        .groupBy((forecast) =>
-          forecast.dt_txt.split(' ').shift()
-        )
-        .map((forecast, date) => {
-          const dayQuarters = every3Hour => _.includes(goodTimes, every3Hour.dt_txt.split(' ').pop());
-          return { date, forecast: forecast.filter(dayQuarters) };
-        })
-        .filter(oneDayWeather => oneDayWeather.forecast.length)
-        .value();
-
       return {
         ...state,
-        forecast,
         selectedForecast: _.first(forecastGroupedByDate),
         forecastGroupedByDate,
         city: action.forecast.city,
